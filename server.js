033a9a5b4a3a616e865d28145ab6f000269f905d
Pierrick Naessens <pierrick.naessens62@gmail.com>
2025-07-03 09:18:08 +0200

    fix readfile for big file

--- server.js
+++ server.js
@@ -1,37 +1,48 @@
 const http = require('http');
+const fs = require('fs');
 const { readFileContent, listProjectFiles } = require('./file_utils');
 
 
 const server = http.createServer(async (req, res) => {
-  try {
-    if (req.url === '/files') {
-      const files = await listProjectFiles();
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(files));
-      
-    } else if (req.url.startsWith('/file/')) {
-      const filename = req.url.replace('/file/', '');
-      const content = await readFileContent(filename);
-      
-      if (content) {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(content);
-      } else {
-        res.writeHead(404);
-        res.end('Fichier non trouve');
-      }
-      
-    } else {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end('<h1>Serveur Node.js</h1><a href="/files">Voir les fichiers</a>');
-    }
-  } catch (err) {
-    console.error('Erreur serveur:', err);
-    res.writeHead(500);
-    res.end('Erreur interne');
-  }
+	console.log(`Req: ${req.method} ${req.url}`)
+	try {
+		if (req.url === '/files') {
+			const files = await listProjectFiles();
+			res.writeHead(200, { 'Content-Type': 'application/json' });
+			res.end(JSON.stringify(files));	
+		} 
+
+		else if (req.url.startsWith('/file/')) {
+		const filename = req.url.replace('/file/', '');
+			try {
+				await fs.promises.access(filename);
+				res.writeHead(200, { 'Content-Type': 'text/plain' });
+				const stream = fs.createReadStream(filename);
+				stream.pipe(res);	
+			} catch (err) {
+					res.writeHead(404);
+					res.end('Fichier non trouvee');
+			}
+		}
+
+		else {
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.end('<h1>Serveur Node.js</h1><a href="/files">Voir les fichiers</a>');
+		}
+	} catch (err) {
+		console.error('Erreur serveur:', err);
+		res.writeHead(500);
+		res.end('Erreur interne');
+	}
 });
 
-server.listen(3000, () => {
-  console.log('Serveur sur http://localhost:3000');
+const port = process.argv[2] || 3000;
+const host = process.argv[3] || 'localhost';
+
+server.listen(port, host, () => {
+	console.log(`Serveur sur http://${host}:${port}`);
+	console.log('Routes disponibles:');
+	console.log('- GET / (accueil)');
+	console.log('- GET /files (liste des fichiers)');
+	console.log('- GET /file/<nom> (contenu d\'un fichier)');
 });
